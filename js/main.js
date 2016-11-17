a41addbb8b59bb6af03bdb612845a1bd309b594f
Hou <vivhou@gmail.com>
2016-11-17 09:50:40 -0500

    added file

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// var MOBILE_THRESHOLD = 600;
+
+// function drawFirstGraphic() {
+// 	$GRAPHDIV = $("firstGraphic");
+// 	isMobile = false;;
+// }
+
+// function drawMap() {
+// 	$GRAPHDIV = $("firstGraphic");
+// 	isMobile = false;;
+// }
+
+// function drawTopten() {
+// 	$GRAPHDIV = $("firstGraphic");
+// 	isMobile = false;;
+// }
+
+
+//   if ($GRAPHDIV.width() <= MOBILE_THRESHOLD) {
+//       isMobile = true;
+//   } else {
+//       isMobile = false;
+//     }

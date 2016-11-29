13ffeee50e9925d40756adde0f6be5a35fe277be
Hou <vivhou@gmail.com>
2016-11-29 16:25:40 -0500

    replaced some spanish text

--- js/language_switch.js
+++ js/language_switch.js
@@ -48,11 +48,11 @@ var MEASURES = {
 
 var MEASURES_DROPDOWN = {
   english: [
-    ["prison","the prison population"], 
-    ["prison_pop","the prison population by office"], 
-    ["arrests","arrests"], 
-    ["probation","the probation population"], 
-    ["parole","the parole population"]
+    ["prison","The Prison Population"], 
+    ["prison_pop","The Prison Population by Office"], 
+    ["arrests","Arrests"], 
+    ["probation","The Probation Population"], 
+    ["parole","The Parole Population"]
   ],
   spanish: [
     ["prison","the prison population-spanish"], 
@@ -80,56 +80,83 @@ var MEASURES_TOOLTIP = {
   ]
 }
 
+var TITLE = {
+  english: ["The Lack of Race and Ethnicity Data in Criminal Justice is Dangerous"],
+  spanish: ["Spanish title"]
+}
+
+var HEADERTEXT = {
+  english:  ["<p><strong>No one knows exactly how many Latinos are in prison, on probation or parole, or being arrested.</strong> In this nation that will soon be “majority minority,” there is a serious lack of information on how many Latinos are involved in the criminal justice system. While 40 states report race (e.g. White, Black, Other) in their arrest records, only 15 states report data on ethnicity - and just 12 do it with the frequency needed for it to be significant. </p> <p>Criminal justice system data are used to develop policies that affect <a href=\"http:\/\/www.urban.org\/research-area\/courts-and-sentencing\" target=\"_blank\">sentencing</a>, <a href=\"http:\/\/www.urban.org\/urban-wire\/prison\-programming-works-so-why-do-we-have-so-little\" target=\"_blank\">prison</a> and <a href=\"http:\/\/www.urban.org\/urban-wire\/prison-programming-works-so-why-do-we-have-so-little\" target=\"_blank\">jail programming</a>, and <a href=\"http:\/\/apps.urban.org\/features\/safer-return\/\" target=\"_blank\">reentry services</a>. Without an accurate picture of who is in our justice systems, we cannot make policy that is appropriate for each state’s population. Community members and advocates cannot know how much of a problem mass incarceration is for Latinos, and ethnic disparities cannot be accurately tracked.</p>"],
+spanish: ["Spanish header text"]
+
+}
 
 
 
 
-$("#spanish")
+
+// $("#spanish")
+// .on("click", function() {
+//   GLOBAL_LANGUAGE = "spanish";
+//   console.log(GLOBAL_LANGUAGE)
+//   d3.select(this).classed("active", true)
+//   if ($(".reg_button").text() == $(".reg_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"])) {
+//     $(".reg_button").text($(".reg_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"]));
+//   } else {
+//     $(".reg_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"], $(".reg_button").text());
+//     $(".reg_button").text(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"])
+//    d3.select(".reg_button").classed(".step1_button", "true")
+//   }
+// })
+
+// $("#spanish")
+// .on("click", function() {
+//    GLOBAL_LANGUAGE = "spanish";
+//   d3.select(this).classed("active", true)
+//   if ($(".all_button").text() == $(".all_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"])) {
+//     console.log('hi')
+//     $(".all_button").text($(".all_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"]));
+//   } else {
+//     console.log('hi')
+//     $(".all_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"], $(".all_button").text());
+//     $(".all_button").text(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"])
+//    d3.select(".all_button").classed(".step1_button", "true")
+//   }
+// })
+
+
+$("#english")
 .on("click", function() {
-  GLOBAL_LANGUAGE = "spanish";
-  console.log(GLOBAL_LANGUAGE)
+    GLOBAL_LANGUAGE = d3.select(this).attr("id");
+    console.log(GLOBAL_LANGUAGE)
+
+  var measures = $('.measure_type')
   d3.select(this).classed("active", true)
-  if ($(".reg_button").text() == $(".reg_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"])) {
-    $(".reg_button").text($(".reg_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"]));
-  } else {
-    $(".reg_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"], $(".reg_button").text());
-    $(".reg_button").text(BUTTON_NAMES[GLOBAL_LANGUAGE]["regular"])
-   d3.select(".reg_button").classed(".step1_button", "true")
-  }
+ // d3.selectAll(".measure_type").each(function(d, i) {
+  for (var i=0; i<measures.length; i++){
+      console.log(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1])
+      console.log(measures.eq(i).text())
+       if (measures.eq(i).text() == (MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1])) {
+        
+        console.log('1')
+    //   } else {
+    //     $(".measure_type").data(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1], $(".measure_type").text());
+    //     $(".measure_type").text(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1])
+    //     console.log(  $(".measure_type").text())
+    }
+    else {
+      measures.eq(i).data(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1]), measures.eq(i).text();
+      measures.eq(i).text(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1])
+    }
+  } 
+  $('#heroimage').find('h1').html(TITLE[GLOBAL_LANGUAGE])
+   $('#header_text').html(HEADERTEXT[GLOBAL_LANGUAGE])
+
+console.log(TITLE[GLOBAL_LANGUAGE])
 })
 
-$("#spanish")
-.on("click", function() {
-   GLOBAL_LANGUAGE = "spanish";
-  d3.select(this).classed("active", true)
-  if ($(".all_button").text() == $(".all_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"])) {
-    console.log('hi')
-    $(".all_button").text($(".all_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"]));
-  } else {
-    console.log('hi')
-    $(".all_button").data(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"], $(".all_button").text());
-    $(".all_button").text(BUTTON_NAMES[GLOBAL_LANGUAGE]["all"])
-   d3.select(".all_button").classed(".step1_button", "true")
-  }
-})
-
-$("#spanish")
-.on("click", function() {
-    GLOBAL_LANGUAGE = "spanish";
- 
-  d3.select(this).classed("active", true)
-  d3.selectAll(".measure_type").each(function(d, i) {
-    if ($(".measure_type").text() == $(".measure_type").data(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1])) {
-      $(".measure_type").text($(".measure_type").data(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1]));
-    } else {
-      $(".measure_type").data(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1], $(".measure_type").text());
-      $(".measure_type").text(MEASURES_DROPDOWN[GLOBAL_LANGUAGE][i][1])
-  }
 
 
-  })
-
-})
 
 // $("#spanish")
 // .on("click", function() {

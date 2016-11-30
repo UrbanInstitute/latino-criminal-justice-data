57580765226fb73a78cacaaace2ec477edb526fc
Hou <vivhou@gmail.com>
2016-11-30 09:11:50 -0500

    added translation change for header

--- js/language_switch.js
+++ js/language_switch.js
@@ -86,9 +86,26 @@ var TITLE = {
 }
 
 var HEADERTEXT = {
-  english:  ["<p><strong>No one knows exactly how many Latinos are in prison, on probation or parole, or being arrested.</strong> In this nation that will soon be “majority minority,” there is a serious lack of information on how many Latinos are involved in the criminal justice system. While 40 states report race (e.g. White, Black, Other) in their arrest records, only 15 states report data on ethnicity - and just 12 do it with the frequency needed for it to be significant. </p> <p>Criminal justice system data are used to develop policies that affect <a href=\"http:\/\/www.urban.org\/research-area\/courts-and-sentencing\" target=\"_blank\">sentencing</a>, <a href=\"http:\/\/www.urban.org\/urban-wire\/prison\-programming-works-so-why-do-we-have-so-little\" target=\"_blank\">prison</a> and <a href=\"http:\/\/www.urban.org\/urban-wire\/prison-programming-works-so-why-do-we-have-so-little\" target=\"_blank\">jail programming</a>, and <a href=\"http:\/\/apps.urban.org\/features\/safer-return\/\" target=\"_blank\">reentry services</a>. Without an accurate picture of who is in our justice systems, we cannot make policy that is appropriate for each state’s population. Community members and advocates cannot know how much of a problem mass incarceration is for Latinos, and ethnic disparities cannot be accurately tracked.</p>"],
-spanish: ["Spanish header text"]
+  english:  ["<p><strong>No one knows exactly how many Latinos are in prison, on probation or parole, or being arrested.</strong> In this nation that will soon be “majority minority,” there is a serious lack of information \
+  on how many Latinos are involved in the criminal justice system. While 40 states report race (e.g. White, Black, Other) in their arrest records, only 15 states report data on ethnicity - and just 12 do it with the frequency \
+  needed for it to be significant. </p> <p>Criminal justice system data are used to develop policies that affect <a href=\"http:\/\/www.urban.org\/research-area\/courts-and-sentencing\" target=\"_blank\">sentencing</a>,\
+   <a href=\"http:\/\/www.urban.org\/urban-wire\/prison\-programming-works-so-why-do-we-have-so-little\" target=\"_blank\">prison</a> and <a href=\"http:\/\/www.urban.org\/urban-wire\/prison-programming-works-so-why-do-we-have-so-\
+   little\" target=\"_blank\">jail programming</a>, and <a href=\"http:\/\/apps.urban.org\/features\/safer-return\/\" target=\"_blank\">reentry services</a>. Without an accurate picture of who is in our justice systems, we cannot make \
+   policy that is appropriate for each state’s population. Community members and advocates cannot know how much of a problem mass incarceration is for Latinos, and ethnic disparities cannot be accurately tracked.</p>  <p>A state’s failure \
+   to report ethnicity in addition to race can even mask racial disparities in the criminal justice system. A state that only counts people as \“black\” or \“white\” <a href=\"http:\/\/www.sentencingproject.org\/wp-content\/uploads\/2016\/06\/The-Color-of-Justice-Racial-and-Ethnic-Disparity-in-State-Prisons.pdf\" \
+   target=\"_blank\">would likely label most of their Latino prison population as \"white.\”</a> Artificially inflating the number of \“white\” people in prison would make white\/black disparities appear less extreme than they actually are. Comprehensive \
+   data around ethnicity doesn’t just affect Latinos.  </p><p>The most informative – and therefore the most useful – data should be: <ul><li><strong>Publicly available:</strong> For data to be useful to policymakers, the general public, \
+   and advocates, it must be publicly accessible.</li><li><strong>Regular and recent:</strong> Data reported regularly (monthly, annually or biannually) are most valuable, as they provide up-to-date counts and demonstrate changes in populations.</li>\
+   <li><strong>Reports race and ethnicity: </strong> At a minimum, demographic data should include a category for Latinos. Ideally, all states would track both race and ethnicity as is done by the <a href=\"http:\/\/www.census.gov\/prod\/cen2010\/briefs\/c2010br-02.pdf\" \
+   target=\"_blank\">US Census</a>, and report them as a combined category (e.g. ‘Hispanic white’ and ‘non-Hispanic black’). </li> </ul></p><p>While our research does not consider how data is collected, race and ethnicity data should be self-reported, \
+   not determined by state employees (e.g. intake workers, police officers, and corrections officials).</p>"],
+  spanish: ["Spanish header text"]
 
+}
+
+var FIRSTSECTION {
+  english:[],
+  spanish:["first section- spanish text"]
 }
 
 
@@ -125,7 +142,7 @@ spanish: ["Spanish header text"]
 // })
 
 
-$("#english")
+$("#spanish")  //$(".language_option")  isn't working
 .on("click", function() {
     GLOBAL_LANGUAGE = d3.select(this).attr("id");
     console.log(GLOBAL_LANGUAGE)
